4ed54b481b35f1406a49e5b12ad8c2d9c0610f44
itsbrandond <brandon@hgraph.com>
2025-03-05 14:17:08 -0500

    adding docs search and fix nav

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,17 +64,13 @@ const config: Config = {
         srcDark: 'img/Hgraph-Logomark_White.svg'
       },
       items: [
-        {to: '/category/faqs', label: 'FAQ', position: 'left'},
-        {to: '/support', label: 'Support', position: 'left'},
-        {
-          href: 'https://hgraph.com/pricing',
-          label: 'Pricing',
-          position: 'left',
-        },
-        {to: '/overview/contact', label: 'Contact', position: 'left'},
+        {to: 'https://hgraph.com/enterprise', label: 'Enterprise', position: 'left'},
+        {to: 'https://hgraph.com/hedera', label: 'Hedera APIs', position: 'left'},
+        {to: '/overview/pricing', label: 'Pricing', position: 'left'},
+        {to: 'https://www.hgraph.com/contact', label: 'Contact', position: 'left'},
         {
           href: 'https://dashboard.hgraph.com',
-          label: 'Account Dashboard',
+          label: 'Account',
           position: 'right',
         },
         
@@ -157,6 +153,38 @@ const config: Config = {
       defaultMode: 'dark',
       disableSwitch: false,
       respectPrefersColorScheme: false,
+    },
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'GTV3VK7V3B',
+
+      // Public API key: it is safe to commit it
+      apiKey: '861faf9a1d7bb4f35f45e57641462567',
+
+      indexName: 'hgraph',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      //... other Algolia params
     },
   } satisfies Preset.ThemeConfig,
 };
